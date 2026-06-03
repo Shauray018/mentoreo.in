0b3060319e211276b5c333fb73e9cd579d2d664d
Shauray018 <shauraydhingra03@gmail.com>
2026-06-04 01:18:04 +0530

    fixed build errors

--- landing-page/app/our-story/page.tsx
+++ landing-page/app/our-story/page.tsx
@@ -1,23 +1,10 @@
-"use client"
-
-import ScrollStack from "@/components/ScrollStack"
-import { ScrollStackItem } from "@/components/ScrollStack"
+import { Navigation } from "../components/Navigation";
 
 export default function OurStory() { 
-    return ( 
-            <ScrollStack className='h-120 rounded-3xl overflow-hidden'>
-            <ScrollStackItem>
-              <h2>Card 1</h2>
-              <p>This is the first card in the stack</p>
-            </ScrollStackItem>
-            <ScrollStackItem>
-              <h2>Card 2</h2>
-              <p>This is the second card in the stack</p>
-            </ScrollStackItem>
-            <ScrollStackItem>
-              <h2>Card 3</h2>
-              <p>This is the third card in the stack</p>
-            </ScrollStackItem>
-          </ScrollStack>
-    )
+  return (
+    <div className="flex justify-center flex-col">
+      <Navigation />
+      hello this is our story page
+    </div>
+  ) 
 }
